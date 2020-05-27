dc3a1be10e5c395de71a4e4576c6c9a93f59aabf
Holly Solomon <hollysolomon@Hollys-Mac.local>
2020-05-27 17:08:19 +0100

    identifying_components

--- read_netlist.cpp
+++ read_netlist.cpp
@@ -22,6 +22,36 @@ int main()
         }
     }
 
+    for (const vector<string> &a: data)
+    {
+     //   cout<< a[0] << endl; 
+        if(a[0].find("V") != string::npos){
+            cout<< "Creating Voltage Source" << endl;
+        }else if(a[0].find("I") != string::npos){
+            cout<< "Creating Current Source" << endl;
+        }else if(a[0].find("*") != string::npos){
+            cout<< "Comment" << endl;
+        }else if(a[0].find("R") != string::npos){
+            cout<< "Creating Resistor" << endl;
+        }else if(a[0].find("C") != string::npos){
+            cout<< "Creating Capacitor" << endl;
+        }else if(a[0].find("L") != string::npos){
+            cout<< "Creating Inductor" << endl;
+        }else if(a[0].find("D") != string::npos){
+            cout<< "Creating Diode" << endl;
+        }else if(a[0].find("Q") != string::npos){
+            cout<< "Creating Transistor" << endl;
+
+        }else if(a[0].find(".tran") != string::npos){
+            cout<< "transient simulation" << endl;
+        }else if(a[0].find(".end") != string::npos){
+            cout<< "end" << endl;
+
+        }else{
+            cout<< "Error - unidentified component or simulation: " << a[0] << endl;
+        }
+    
+    }
     /* this code just prints the netlist out, like a test
     for (const auto &a: data)
     {
@@ -32,5 +62,6 @@ int main()
         cout<<'\n';
     }
      */   
+  
 }
     
