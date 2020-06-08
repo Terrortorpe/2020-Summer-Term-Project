964ebef7264b5df3c7b045e963cecd9c484c477f
Barabas <pab5118@ic.ac.uk>
2020-06-08 18:29:02 +0200

    Halfway

--- Summertry2.cpp
+++ Summertry2.cpp
@@ -1,11 +1,17 @@
 #include <vector>
 #include "Eigen/Dense"
 #include <complex>
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 typedef Eigen::MatrixXcd Matrix;
 typedef Eigen::VectorXcd Vector;
 complex<double> I = {0,1};
+map<string,node*> Pair;
 
 
 
@@ -202,7 +208,71 @@ class currentsourcenode :public componentnode{
     }
 };*/
 
+// Function for searching a tyingnode by name or creating it if it doenst exist yet
+
+node* Gettyingnode(string name){
+    node* res = Pair[name];
+    if (res == nullptr){
+        Pair[name] = res = new tyingnode(); //fancy syntax for a new tyingnode named pair which is equal to the name of Pair
+    }
+    return res;
+}
+
 int main()
 {
+
+    //Reading in 
+
+    ifstream something("netlist.txt");
+    string netlist_line;
+    while (getline(something,netlist_line))
+    {
+        string component, nodefrom, nodeto;
+        istringstream iss(netlist_line);
+        iss >> component >> nodefrom >> nodeto;
+        node* tyingfrom = Gettyingnode(nodefrom);
+        node* tyingto = Gettyingnode(nodeto);
+        node* componentnodepointer;
+        switch(component[0]){
+            case 'V':
+            componentnodepointer = new voltagesourcenode();
+            break;
+            case 'I':
+            componentnodepointer = new currentsourcenode();
+            break;
+            case 'R':
+            componentnodepointer = new resistornode();
+            break;
+            case 'L':
+            componentnodepointer = new Inductornode(); // need constructor
+            break;
+            case 'C':
+            componentnodepointer = new Capacitornode(); // need constructor
+            break;
+        }
+    }
+
+    //Solving the matrix of doom
+
+    Matrix mat;
+    Vector vec;
+    Vector res;
+    res = mat.bdcSvd(Eigen::ComputeThinU|Eigen::ComputeThinV).solve(vec);
+    cout << res << endl;
     return 0;
 }
+
+
+/* 
+A test circuit to demonstrate SPICE syntax 
+V1 N003 0 SINE(2 1 1000)
+R1 N001 N003 1k
+C1 N001 0 1μ
+I1 0 N004 0.1
+D1 N004 N002 D
+L1 N002 N001 1m
+R2 N002 N001 1Meg
+Q1 N003 N001 0 NPN
+.tran 0 10ms 0 1us
+.end 
+*/
