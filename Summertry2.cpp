54582bdd6b48a6af1d191bf7a6d0754273044a63
Barabas <pab5118@ic.ac.uk>
2020-06-11 21:25:48 +0200

    Even more

--- Summertry2.cpp
+++ Summertry2.cpp
@@ -6,58 +6,120 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <regex>
+
 using namespace std;
+
+
+class componentnode;
+class tyingnode;
 
 typedef Eigen::MatrixXcd Matrix;
 typedef Eigen::VectorXcd Vector;
+
 complex<double> I = {0,1};
+
 map<string,tyingnode*> PairT;
 map<string,componentnode*> PairC;
+
+vector<edge*> Edges;
+//Indices for voltages and currents
 int IDcount;
-double omega;
+map<int, string> id_name;
+//Indices for derivatives
+int dIDcount;
+//double ac_omega;
 
+struct edge {
+    componentnode* A;
+    tyingnode* B;
+    //Cache from previous result
+    double Current = 0; //positive if going INTO the tyingnode
+    int CurrentID;
+    int dCurrentID;
 
+    edge(componentnode* a_, tyingnode* b_){
+        A=a_;
+        B=b_;
+        CurrentID =  IDcount;
+        id_name[CurrentID] = A->name + "->" + B->name;
+        IDcount++;
+        dCurrentID =  dIDcount;
+        dIDcount++;
+        A->edges.push_back(this);
+        B->edges.push_back(this);
 
-struct edge{
-componentnode* A;
-tyingnode* B;
-double Current; //positive if going INTO the tyingnode
-int CurrentID;
+        Edges.push_back(this);
+    }
 
-edge(componentnode* a_, tyingnode* b_){
-    A=a_;
-    B=b_;
-    CurrentID =  IDcount;
-    IDcount++;
-    A->edges.push_back(this);
-    B->edges.push_back(this);
-}
+    void cache_prev_result(double dt, Vector& res) {
+        Current = res(CurrentID);
+    }
 };
 
-class node{
-
+class node {
 public:
-   vector<edge*> edges;
-    virtual void constraint(Matrix& mat){
+    string name;
+    vector<edge*> edges;
+    node(string _name) : name(_name) {
+
+    }
+    virtual void constraint_cons(Matrix& mat, Vector& vec, double t){
+        throw 1;
+    }
+
+    virtual void constraint_td(Matrix& mat, Vector& vec, double t){
+        throw 1;
+    }
+
+    virtual void cache_prev_result(double dt, Vector& res) {
         throw 1;
     }
 };
 
-class componentnode :public node{
-    virtual void constraint(Matrix& mat){
+class componentnode : public node {
+public:
+    componentnode(string _name) : node(_name) {
+
+    }
+
+    virtual void constraint_cons(Matrix& mat, Vector& vec, double t){
+        throw 1;
+    }
+
+    virtual void constraint_td(Matrix& mat, Vector& vec, double t){
+        throw 1;
+    }
+
+    /*virtual void prepare_ac_analysis(double omega) {
+        throw 1;
+    }
+
+    virtual void prepare_dc_analysis() {
+        throw 1;
+    }*/
+
+    virtual void cache_prev_result(double dt, Vector& res) {
         throw 1;
     }
 };
 
-class tyingnode :public node{
-
+class tyingnode : public node {
 public:
-    double Voltage;
+    double Voltage = 0;
     int VoltageID;
-    
+    int dVoltageID;
 
-    // Currents have to sum to 0
-    void constraint(Matrix& mat, Vector& vec){
+    tyingnode(string _name) : node(_name) {
+        VoltageID = IDcount;
+        id_name[VoltageID] = name;
+        IDcount++;
+        dVoltageID = IDcount;
+        IDcount++;
+    }
+    
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
+        //Sum current
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
         for(auto && edge:edges){
@@ -65,18 +127,34 @@ public:
         }
         vec(vec.size()-1) = 0;
     }
+
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+        //Sum current derivative
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        for(auto && edge:edges){
+            mat(mat.rows()-1, edge->dCurrentID) = 1;
+        }
+        vec(vec.size()-1) = 0;
+    }
+
+    void cache_prev_result(double dt, Vector& res) {
+        Voltage = res(VoltageID);
+    }
 };
 
 class Capacitornode :public componentnode{
     public:
     double Capacitance;
-    complex<double> Impedance;
+    //complex<double> Impedance;
 
-    Capacitornode(stringstream &ss){
-
+    Capacitornode(string _name, stringstream &ss) : componentnode(_name) {
+        string s;
+        s = ss.str();
+        Capacitance = ReadValue(s);
     }
 
-    void constraint(Matrix& mat, Vector& vec){
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
 
         //Current flowing in and out has to sum to 0
         mat.resize(mat.rows()+1,mat.cols());
@@ -86,14 +164,37 @@ class Capacitornode :public componentnode{
         }
         vec(vec.size()-1) = 0;
 
-
+        //Act as voltage source
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
-        mat(mat.rows()-1, edges[0]->B->VoltageID) = 1;      // V(0)-----E(0)-----R------E(1)----V(1)    V(0)-V(1)+current*resistancce
-        mat(mat.rows()-1, edges[1]->B->VoltageID) = -1;
-        mat(mat.rows()-1, edges[0]->CurrentID) = Impedance;
+        mat(mat.rows()-1, edges[0]->B->VoltageID) = -1;
+        mat(mat.rows()-1, edges[1]->B->VoltageID) = 1;          //V(0)----E(0)-----Source(U)------E(1)-------V(1)
+        vec(vec.size()-1) = edges[1]->B->Voltage - edges[0]->B->VoltageID;
+    }
+
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+        //Current derivative sums to 0
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        for(auto && edge:edges){
+            mat(mat.rows()-1, edge->CurrentID) = 1;
+        }
         vec(vec.size()-1) = 0;
 
+        //V' = CI
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        mat(mat.rows()-1, edges[0]->B->dVoltageID) = 1.0;      // V(0)-----E(0)-----R------E(1)----V(1)    V(0)-V(1)+current*resistancce
+        mat(mat.rows()-1, edges[1]->B->dVoltageID) = -1.0;
+        mat(mat.rows()-1, edges[0]->CurrentID) = Capacitance;
+        vec(vec.size()-1) = 0;
+    }
+
+    /*void prepare_ac_analysis(double omega) {
+        Impedance = 1.0/(I * omega * Capacitance);
+    }*/
+
+    void cache_prev_result(double dt, Vector& res) {
 
     }
 };
@@ -101,13 +202,15 @@ class Capacitornode :public componentnode{
 class Inductornode :public componentnode{
     public:
     double Inductance;
-    complex<double> Impedance;
+    //complex<double> Impedance;
 
-    Inductornode(stringstream &ss){
-
+    Inductornode(string _name, stringstream &ss) : componentnode(_name) {
+        string s;
+        s = ss.str();
+        Inductance = ReadValue(s);
     }
 
-    void constraint(Matrix& mat, Vector& vec){
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
 
         //Current flowing in and out has to sum to 0
         mat.resize(mat.rows()+1,mat.cols());
@@ -117,14 +220,38 @@ class Inductornode :public componentnode{
         }
         vec(vec.size()-1) = 0;
 
+        //Current stays constant
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        mat(mat.rows()-1, edges[0]->CurrentID) = 1;
+        vec(vec.size()-1) = edges[0]->Current;
+    }
 
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+        //Current derivative flowing in and out has to sum to 0
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        for(auto && edge:edges){
+            mat(mat.rows()-1, edge->dCurrentID) = 1;
+        }
+        vec(vec.size()-1) = 0;
+
+        //V = L*I'
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
         mat(mat.rows()-1, edges[0]->B->VoltageID) = 1;
         mat(mat.rows()-1, edges[1]->B->VoltageID) = -1;
-        mat(mat.rows()-1, edges[0]->CurrentID) = Impedance;
+        mat(mat.rows()-1, edges[0]->dCurrentID) = Inductance;
         vec(vec.size()-1) = 0;
 
+
+    }
+
+    /*void prepare_ac_analysis(double omega) {
+        Impedance = I * omega * Inductance;
+    }*/
+
+    void cache_prev_result(double dt, Vector& res) {
 
     }
 };
@@ -132,11 +259,13 @@ class Inductornode :public componentnode{
 class resistornode :public componentnode{
     public:
     double Resistance;
-    resistornode(stringstream &ss){
-        
+    resistornode(string _name, stringstream &ss) : componentnode(_name) {
+        string s;
+        s = ss.str();
+        Resistance = ReadValue(s);
     };
 
-    void constraint(Matrix& mat, Vector& vec){
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
 
         //Current flowing in and out has to sum to 0
         mat.resize(mat.rows()+1,mat.cols());
@@ -146,14 +275,38 @@ class resistornode :public componentnode{
         }
         vec(vec.size()-1) = 0;
 
-
+        //V = I R
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
         mat(mat.rows()-1, edges[0]->B->VoltageID) = 1;
         mat(mat.rows()-1, edges[1]->B->VoltageID) = -1;
         mat(mat.rows()-1, edges[0]->CurrentID) = Resistance;    // V(0)-----E(0)-----R------E(1)----V(1)    V(0)-V(1)+current*resistance=0
         vec(vec.size()-1) = 0;
+    }
 
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+        //Current derivative flowing in and out has to sum to 0
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        for(auto && edge:edges){
+            mat(mat.rows()-1, edge->dCurrentID) = 1;
+        }
+        vec(vec.size()-1) = 0;
+
+        //V' = I' R
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        mat(mat.rows()-1, edges[0]->B->dVoltageID) = 1;
+        mat(mat.rows()-1, edges[1]->B->dVoltageID) = -1;
+        mat(mat.rows()-1, edges[0]->dCurrentID) = Resistance;    // V(0)-----E(0)-----R------E(1)----V(1)    V(0)-V(1)+current*resistance=0
+        vec(vec.size()-1) = 0;
+    }
+
+    /*void prepare_ac_analysis(double omega) {
+        
+    }*/
+
+    void cache_prev_result(double dt, Vector& res) {
 
     }
 };
@@ -161,15 +314,35 @@ class resistornode :public componentnode{
 
 class voltagesourcenode :public componentnode{
     public:
-    double Voltage;
+    bool time_dep = false;
+    double Offset;
+    double Amplitude;
+    double Frequency;
 
-    voltagesourcenode(stringstream &ss){
-        
+    voltagesourcenode(string _name, stringstream &ss) : componentnode(_name) {
+        string s;
+        s = ss.str();
+        if(s.rfind("SINE") == 0){
+            regex rgx("[(](.*)[)]");
+            smatch match;
+            if (regex_search(s.begin(), s.end(), match, rgx)) {
+                ss.clear();
+                ss << match[1];
+                ss >> Offset >> Amplitude >> Frequency;
+                time_dep = true;
+            } else {
+                throw 1;
+            }
+        } else {
+            Offset = 0;
+            Amplitude = ReadValue(s);
+            Frequency = 0;
+            time_dep = false;
+        }
     }
 
 
-    void constraint(Matrix& mat, Vector& vec){
-
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
         //Current flowing in and out has to sum to 0
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
@@ -183,31 +356,85 @@ class voltagesourcenode :public componentnode{
         vec.resize(vec.size()+1);
         mat(mat.rows()-1, edges[0]->B->VoltageID) = -1;
         mat(mat.rows()-1, edges[1]->B->VoltageID) = 1;          //V(0)----E(0)-----Source(U)------E(1)-------V(1)
-        vec(vec.size()-1) = Voltage;                            //V(1)-V(0)=U
+        vec(vec.size()-1) = time_dep ? (Offset + Amplitude * sin(Frequency * 2 * 3.1415926535897932 * t)) : Amplitude; //V(1)-V(0)=U
+    }
 
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+        //Current derivative flowing in and out has to sum to 0
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        for(auto && edge:edges){
+            mat(mat.rows()-1, edge->dCurrentID) = 1;
+        }
+        vec(vec.size()-1) = 0;
+
+        //Voltage derivative must match
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        mat(mat.rows()-1, edges[0]->B->dVoltageID) = -1;
+        mat(mat.rows()-1, edges[1]->B->dVoltageID) = 1;          //V(0)----E(0)-----Source(U)------E(1)-------V(1)
+        vec(vec.size()-1) = 0; //V'(1)-V'(0)=0
+    }
+
+    void cache_prev_result(double dt, Vector& res) {
 
     }
 };
 
 class currentsourcenode :public componentnode{
-    public:
-    double Current;
+public:
+    bool time_dep = false;
+    double Offset;
+    double Amplitude;
+    double Frequency;
 
-    currentsourcenode(stringstream &ss){
-        
+    currentsourcenode(string _name, stringstream &ss) : componentnode(_name) {
+        string s;
+        s = ss.str();
+        if(s.rfind("SINE") == 0){
+            regex rgx("[(](.*)[)]");
+            smatch match;
+            if (regex_search(s.begin(), s.end(), match, rgx)) {
+                ss.clear();
+                ss << match[1];
+                ss >> Offset >> Amplitude >> Frequency;
+                time_dep = true;
+            } else {
+                throw 1;
+            }
+        } else {
+            Offset = 0;
+            Amplitude = ReadValue(s);
+            Frequency = 0;
+            time_dep = false;
+        }
     }
-    void constraint(Matrix& mat, Vector& vec){
+    void constraint_cons(Matrix& mat, Vector& vec, double t){
 
         //Current source makes current flow from the 0 to the 1
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
         mat(mat.rows()-1, edges[0]->CurrentID) = 1;
-        vec(vec.size()-1) = -Current;
+        vec(vec.size()-1) = -time_dep ? (Offset + Amplitude * sin(Frequency * 2 * 3.1415926535897932 * t)) : Amplitude;
 
         mat.resize(mat.rows()+1,mat.cols());
         vec.resize(vec.size()+1);
         mat(mat.rows()-1, edges[1]->CurrentID) = 1;
-        vec(vec.size()-1) = Current;
+        vec(vec.size()-1) = time_dep ? (Offset + Amplitude * sin(Frequency * 2 * 3.1415926535897932 * t)) : Amplitude;
+    }
+
+    void constraint_td(Matrix& mat, Vector& vec, double t){
+
+        //Current changes sum to 0
+        mat.resize(mat.rows()+1,mat.cols());
+        vec.resize(vec.size()+1);
+        mat(mat.rows()-1, edges[0]->dCurrentID) = 1;
+        mat(mat.rows()-1, edges[1]->dCurrentID) = -1;
+        vec(vec.size()-1) = time_dep ? (Offset + Amplitude * sin(Frequency * 2 * 3.1415926535897932 * t)) : Amplitude;
+    }
+
+    void cache_prev_result(double dt, Vector& res) {
+
     }
 };
 
@@ -243,19 +470,54 @@ class currentsourcenode :public componentnode{
 tyingnode* Gettyingnode(string name){
     tyingnode* res = PairT[name];
     if (res == nullptr){
-        PairT[name] = res = new tyingnode(); //fancy syntax for a new tyingnode named pair which is equal to the name of Pair
+        PairT[name] = res = new tyingnode(name); //fancy syntax for a new tyingnode named pair which is equal to the name of Pair
     }
     return res;
 }
 
 double ReadValue(string s){
-// To Do
-// Gets the double value from the string
+    double x,y;
+    x = stod(s);
+    if (s.find("k"))
+    {
+        y = x * 1e3;
+        return y;
+    }
+    else if (s.find("Meg"))
+    {
+        y = x * 1e6;
+        return y;
+    }
+    else if (s.find("μ"))
+    {
+        y = x * 1e-6;
+        return y;
+    }
+    else if (s.find("m"))
+    {
+        y = x * 1e-3;
+        return y;
+    }
+    else if (s.find("p"))
+    {
+        y = x * 1e-12;
+        return y;
+    }
+    else if(s.find("n"))
+    {
+        y = x * 1e-9;
+        return y;
+    }
+    else
+    {
+        return x;
+    }
 }
 
 int main()
 {
-
+    bool transient;
+    
     //Reading in 
 
     ifstream something("netlist.txt");
@@ -270,19 +532,19 @@ int main()
         componentnode* componentnodepointer;
         switch(component[0]){
             case 'V':
-            componentnodepointer = new voltagesourcenode(iss);
+            componentnodepointer = new voltagesourcenode(component, iss);
             break;
             case 'I':
-            componentnodepointer = new currentsourcenode(iss);
+            componentnodepointer = new currentsourcenode(component, iss);
             break;
             case 'R':
-            componentnodepointer = new resistornode(iss);
+            componentnodepointer = new resistornode(component, iss);
             break;
             case 'L':
-            componentnodepointer = new Inductornode(iss); // need constructor
+            componentnodepointer = new Inductornode(component, iss);
             break;
             case 'C':
-            componentnodepointer = new Capacitornode(iss); // need constructor
+            componentnodepointer = new Capacitornode(component, iss);
             break;
         }
         new edge(componentnodepointer, tyingfrom);
@@ -292,20 +554,61 @@ int main()
 
     }
 
-    //Solving the matrix of doom
+    fstream textfile("out.txt");
+ 
+    double stoptime, timestep;
+    textfile.open("simulation_results.txt");
+ 
+    for(double time = 0; time = stoptime; time = time+timestep)
+    {
+        //Step 1: Time-Indep solution
+        {
+            Matrix mat = Matrix(0, IDcount);
+            Vector vec = Vector(0);
+            Vector res = Vector(IDcount);
 
-    Matrix mat;
-    Vector vec;
-    Vector res;
-    res = mat.bdcSvd(Eigen::ComputeThinU|Eigen::ComputeThinV).solve(vec);
-    cout << res << endl;
+            for(auto&& tnode : PairT) {
+                tnode.second->constraint_cons(mat, vec, time);
+            }
+
+            for(auto&& cnode : PairC) {
+                cnode.second->constraint_cons(mat, vec, time);
+            }
+
+            //Solving the matrix of doom
+            res = mat.bdcSvd(Eigen::ComputeThinU|Eigen::ComputeThinV).solve(vec);
+
+            //Cache results
+            for(auto&& tnode : PairT) {
+                tnode.second->cache_prev_result(timestep, res);
+            }
+
+            for(auto&& cnode : PairC) {
+                cnode.second->cache_prev_result(timestep, res);
+            }
+
+            for(auto&& edge : Edges) {
+                edge->cache_prev_result(timestep, res);
+            }
+
+            textfile << time << ":" << res <<endl;
+        }
+        //Time se
+        {
+
+        }
+
+    }
+
     return 0;
+
 }
 
 
 /* 
 A test circuit to demonstrate SPICE syntax 
 V1 N003 0 SINE(2 1 1000)
+V2 N005 0 5
 R1 N001 N003 1k
 C1 N001 0 1μ
 I1 0 N004 0.1
@@ -316,4 +619,7 @@ Q1 N003 N001 0 NPN
 .tran 0 10ms 0 1us
 .end 
 */
+
+
+
 
